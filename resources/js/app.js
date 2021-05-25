80db53ccd6f55e965916e2b3d6c680f29c6e7bd9
Johan Dahl <johan.dahl@consector.se>
2021-05-25 20:02:43 +0200

    Upgrade Statamic + various misc changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import { createApp } from "vue"
 import Ingredients from "./components/Ingredients.vue"
-import Instruction from "./components/Instruction.vue"
 
 const app = createApp({
     components: {
         Ingredients,
-        Instruction,
     }
 });
 
